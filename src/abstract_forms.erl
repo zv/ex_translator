7fd6edb70518c90bad433006e85e1751caa9385f
Zephyr <zephyr.pellerin@gmail.com>
2013-11-18 19:03:47 -0800

    abstract_forms now exports method to translate module from abstract forms

--- src/abstract_forms.erl
+++ src/abstract_forms.erl
@@ -5,12 +5,7 @@
                      attribute_arguments/1,
                      type/1
                     ]).
--export([
-         build_initial_context/2,
-         build_function_tree/2,
-         function_arguments/1,
-         process_function/1,
-         translate_expression/2,
+-export([translate_module/2,
          get_module/1,
          get_exports/1,
          get_attribute/2
@@ -18,12 +13,16 @@
 
 -include("ex_translate.hrl").
 
+translate_module(Forms, _Options) ->
+  ExpressionTree = build_function_tree(Forms),
+  build_initial_context(Forms, ExpressionTree).
+
 %%% @doc
 %%% Initializes the module context, returning a proplist with which we begin
 %%% building up the abstract representation intended for consumption by Elixir.
 %%% @end
 -spec build_initial_context(forms(), #elixir_expr{}) -> #elixir_expr{}.
-build_initial_context(Forms, ModuleBody) ->
+build_initial_context(Forms, ExpressionTree) ->
   #elixir_expr{
      qualifier = defmodule,
      metadata  = [ ?ElixirCtx ],
@@ -31,35 +30,39 @@ build_initial_context(Forms, ModuleBody) ->
                   % Module Name
                   { '__aliases__', [{alias, false}], [ get_module(Forms) ]},
                   % Begin the module definiton block
-                  [{do, ModuleBody}]
+                  [{do, ExpressionTree}]
                  ]
     }.
 
 %%% @doc
 %%% Return a list of our function's named arguments.
 %%% @end
--spec function_arguments(form()) -> [string()].
+-spec function_arguments(form()) -> #elixir_expr{}.
 function_arguments(Func) ->
   {clause, _LineNum, Args, _B, _Body} = hd(erl_syntax:function_clause(Func)),
-  [ {Arg, [], 'Elixir'} ||  {var, _, Arg} <- Args ].
+  [ #elixir_expr{qualifier=Arg, metadata=[], arguments='Elixir'} ||  {var, _, Arg} <- Args ].
 
 -spec process_function(form()) -> #elixir_expr{}.
 process_function(Func) ->
   {tree, atom, _, FuncName} = erl_syntax:function_name(Func),
   ArgList = function_arguments(Func),
-  ExpressionTree = [ ex_abstract_forms:parse_expression(type(Expression), Expression) ||
+  ExpressionTree = [ translate_expression(type(Expression), Expression) ||
                      Expression <- erl_syntax:function_clauses(Func) ],
-  {def,
-   [?ElixirCtx],
-   [ {FuncName, [], ArgList } ],
-   [{do, ExpressionTree}]
-  }.
-
+  #elixir_expr{
+     qualifier = def,
+     metadata  = [ ?ElixirCtx ],
+     arguments = [
+                  { FuncName, [], ArgList },
+                  [ { do, ExpressionTree } ]
+                 ]
+    }.
 
 %%% @doc
 %%% Process our forms, appending each function to the initial module context block
 %%% @end
 -spec build_function_tree([any()], forms()) -> #elixir_expr{}.
+build_function_tree(Forms) ->
+  build_function_tree([], Forms).
 build_function_tree(InitialContext, [F|Forms]) ->
   case type(F) == function of
     true  -> process_function(F) ++ build_function_tree(InitialContext, Forms);
@@ -80,20 +83,20 @@ get_exports(Forms) ->
 %%% Retuns an attribute from the parse_transform Form List
 -spec get_attribute(atom(), [any()]) -> undefined | atom().
 get_attribute(A, Forms) ->
-    case find_attribute(A, Forms) of
-        false -> erl_syntax:atom(undefined);
-        Other -> Other
-    end.
+  case find_attribute(A, Forms) of
+    false -> erl_syntax:atom(undefined);
+    Other -> Other
+  end.
 
 %%% Search the form list for an attribute
 -spec find_attribute(atom(), [any()]) -> none | [any()].
 find_attribute(A, [F|Forms]) ->
-    case type(F) == attribute andalso atom_value(attribute_name(F)) == A of
-        true  -> attribute_arguments(F);
-        false -> find_attribute(A, Forms)
-    end;
+  case type(F) == attribute andalso atom_value(attribute_name(F)) == A of
+    true  -> attribute_arguments(F);
+    false -> find_attribute(A, Forms)
+  end;
 find_attribute(_, []) ->
-    false.
+  false.
 
 % Returns the name of the module being compiled.
 -spec get_module([any()]) -> atom().
