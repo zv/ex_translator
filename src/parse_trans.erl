eaa2dbdab1f8644585a93a94f85e3d25e60a14fa
Zephyr <zephyr.pellerin@gmail.com>
2013-11-18 19:04:34 -0800

    parse_trans calls abstract_forms:translate_forms(Forms).

--- src/parse_trans.erl
+++ src/parse_trans.erl
@@ -1,25 +1,14 @@
 %%% This module is a simple mechanism to print and return the parse tree
 -module(parse_trans).
--import(erl_syntax, [atom_value/1,
-                     attribute_name/1,
-                     attribute_arguments/1,
-                     function_clauses/1,
-                     type/1
-                    ]).
+-author("Zephyr Pellerin <zv@nxvr.org>").
 -include("ex_translate.hrl").
 -export([parse_transform/2]).
 
-% A convienence method for performing functions on the nodes in a postorder fashion.
-postorder(F, Tree) ->
-  F(case erl_syntax:subtrees(Tree) of
-      [] -> Tree;
-      List ->  [[postorder(F, Subtree) || Subtree <- Group] || Group <- List]
-    end).
-
 parse_transform(Forms, Options) ->
-    io:fwrite("Options = ~p~n", [Options]),
-    % io:fwrite("Forms = ~p~n", [Forms]),
+    %io:fwrite("Options = ~p~n", [Options]),
+    %io:fwrite("Forms = ~p~n", [Forms]),
     Module = abstract_forms:get_module(Forms),
-    [ io:fwrite("~p~n~n~n~n~n", [Subtree]) || Subtree <- Forms ],
+    TranslatedModule = abstract_forms:translate_module(Forms, Options),
+    io:fwrite("TranslatedModule = ~p~n", [TranslatedModule]),
     io:fwrite("Module = ~p~n", [Module]),
     Module.
