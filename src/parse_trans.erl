f2002f7410f0dd9162dbb5fd87765beb033d9eb0
Zephyr <zephyr.pellerin@gmail.com>
2013-11-25 19:52:59 -0800

    Clearer argeparser code

--- src/parse_trans.erl
+++ src/parse_trans.erl
@@ -5,14 +5,14 @@
 -export([parse_transform/2]).
 
 parse_transform(Forms, Options) ->
-    %io:fwrite("Options = ~p~n", [Options]),
-    %io:fwrite("Forms = ~p~n", [Forms]),
+    io:fwrite("Options = ~p~n", [Options]),
+    io:fwrite("Forms = ~p~n", [Forms]),
     Module = abstract_forms:get_module(Forms),
-    TranslatedModule = try
-      abstract_forms:translate_module(Forms, Options)
-    catch
-      _:_ -> io:format("Backtrace ~p~n", [erlang:get_stacktrace()])
-    end,
-    io:fwrite("TranslatedModule = ~p~n", [TranslatedModule]),
-    io:fwrite("Module = ~p~n", [Module]),
+    % TranslatedModule = try
+    %   abstract_forms:translate_module(Forms, Options)
+    % catch
+    %   _:_ -> io:format("Backtrace ~p~n", [erlang:get_stacktrace()])
+    % end,
+    % io:fwrite("TranslatedModule = ~p~n", [TranslatedModule]),
+    % io:fwrite("Module = ~p~n", [Module]),
     Module.
